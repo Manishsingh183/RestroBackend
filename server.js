a60543cba57e218b4ff0c4c28e48c49d1be50cc7
Manishsingh183 <manishsingh9051612@gmail.com>
2024-06-07 14:45:08 +0530

    Access control allow origin code tried

--- server.js
+++ server.js
@@ -27,11 +27,32 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", [
+    "https://restroproject.netlify.app",
+    "https://master--restroproject.netlify.app",
+  ]);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 const port = 8080;
 const database_URL = process.env.database_URL;
 
